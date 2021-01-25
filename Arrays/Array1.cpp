3369f81e225c64bf342c53fb289dac9eb42b481f
Yuvraj883 <kingyuvi88@gmail.com>
2021-01-25 19:00:04 +0530

    Find the product and sum of the elements within an array

---
+++ Arrays/Array1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, arr[n];
+    int sum = 0, product = 1;
+    cout << "Enter the number of elements you want to enter: ";
+    cin>> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        product = product * arr[i];
+    }
+    cout << "The sum of all the elements in the array is " << sum;
+    cout << endl
+         << "The product of all the elements in the array is " << product;
+    return 0;
+}
