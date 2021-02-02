5b4779463e1f177ed4c1780d35034f3b05dcb903
Yuvraj883 <kingyuvi88@gmail.com>
2021-02-02 13:40:03 +0530

    Dutch National Flag Algo

--- Arrays/10_sort012.cpp
+++ Arrays/10_sort012.cpp
@@ -13,27 +13,22 @@ void sort012(int *arr, int n)
     int low = 0;
     int mid = 0;
     int high = n - 1;
-while (mid<=high)
+    while (mid <= high)
     {
         switch (arr[mid])
         {
         case 0:
             swap(arr[low++], arr[mid++]);
-            
-            
             break;
         case 1:
             mid++;
             break;
         case 2:
             swap(arr[mid], arr[high--]);
-            //high--;
             break;
-     }
-
+        }
     }
-        printArr(arr, n);
-
+    printArr(arr, n);
 }
 
 int main()
