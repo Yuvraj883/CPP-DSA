29a26f87f4531c70241010f2caebb6f1c3f26940
Yuvraj883 <kingyuvi88@gmail.com>
2021-02-02 13:31:29 +0530

    Dutch National Flag Algo

--- Arrays/10_sort012.cpp
+++ Arrays/10_sort012.cpp
@@ -1,36 +1,54 @@
-//Sort an array consisting of 0s,1s and 2s without using any sorting algorythm 
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
-void printArr(int *arr,int n){
-    for (int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+void printArr(int *arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    cout<<endl; 
+    cout << endl;
+}
+void sort012(int *arr, int n)
+{
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+while (mid<=high)
+    {
+        switch (arr[mid])
+        {
+        case 0:
+            swap(arr[low++], arr[mid++]);
+            
+            
+            break;
+        case 1:
+            mid++;
+            break;
+        case 2:
+            swap(arr[mid], arr[high--]);
+            //high--;
+            break;
+     }
+
+    }
+        printArr(arr, n);
+
 }
 
-void sort012(int *arr, int n){
-    for (int i=0; i<n; i++){
-        if (arr[i]>arr[i+1]){
-            swap(arr[i],arr[i+1]);
-        }
-    }
-    sort(arr, arr + n, [](int a, int b){
-	return a < b;
-    });
-}
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-int main(){
-    int n; 
-    cout<<"Enter the size of the array: "; 
-    cin>> n; 
-
-    int arr[n]; 
-    cout<<"Enter the elements of the array: "; 
-    for (int i=0; i<n; i++){
-        cin>>arr[i];
+    int arr[n];
+    cout << "Enter the array elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    sort012(arr,n);     
-    printArr(arr, n);
-    return 0; 
+    sort012(arr, n);
+
+    return 0;
 }
